98779d75d81719c8480485fd824e956c45749a2e
Haotian Tu <HaotianTu@126.com>
2024-06-08 02:27:13 +1000

    Initial Commit

---
+++ NBTEditor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NBTEditor
+//
+//  Created by 屠昊天 on 8/6/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NBTEditor/NBTEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBTEditorApp.swift
+//  NBTEditor
+//
+//  Created by 屠昊天 on 8/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct NBTEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
